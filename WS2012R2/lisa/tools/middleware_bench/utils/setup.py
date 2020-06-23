7c3c84e51813e7ac35ba461f4b7096cd8e0dfc68
Baihua Lu <balu@microsoft.com>
2020-06-23 12:29:56 +0800

    Change iops of aws to 3500

--- WS2012R2/lisa/tools/middleware_bench/utils/setup.py
+++ WS2012R2/lisa/tools/middleware_bench/utils/setup.py
@@ -212,7 +212,7 @@ class SetupTestEnv:
         disk_args = {}
         if self.provider == constants.AWS:
             device = constants.DEVICE_AWS.replace('sd', 'xvd')
-            disk_args['iops'] = 5000
+            disk_args['iops'] = 3500
             disk_args['volume_type'] = self.connector.volume_type['ssd_io1']
             disk_args['device'] = constants.DEVICE_AWS
         elif self.provider == constants.AZURE:
